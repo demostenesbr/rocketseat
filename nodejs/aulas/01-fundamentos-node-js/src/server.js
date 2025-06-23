8da46a3f650809ab4d5ddd323af85f13b238c2b1
Demóstenes Muniz Brito <demostenes@demostenes.com.br>
2025-06-23 00:02:58 -0300

    Feat: Persisting database

--- nodejs/aulas/01-fundamentos-node-js/src/server.js
+++ nodejs/aulas/01-fundamentos-node-js/src/server.js
@@ -1,63 +1,30 @@
-// Criar usuários
-// Listar usuários
-// Edição de usuários
-// Remoção de usuários
-
-// Métodos HTTP => https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Methods
-// Método HTTP
-// URL
-
-// GET, POST, PUT, PATCH, DELETE,
-
-// GET => Buscar uma informação do back-end
-// POST => Enviar uma informação para o back-end
-// PUT => Atualizar uma informação no back-end
-// PATCH => Alterar uma informação no back-end
-// DELETE => Remover uma informação no back-end
-
-// GET => http://localhost:3333/users => Buscar usuários do back-end
-// POST => http://localhost:3333/users => Criar um usuário no back-end
-
-// Statefull - StateLess
-
-// Request Param => Parâmetros que vem na própria rota que identificam um recurso
-
-// HTTP Status Code => https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
-
 import http from "node:http";
+import { Database } from "./database.js";
+import { json } from "./middlewares/json.js";
 
-const users = [];
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  const buffers = [];
-
-  for await (const chunk of req) {
-    buffers.push(chunk);
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    req.body = null;
-  }
+  await json(req, res);
 
   if (method === "GET" && url === "/users") {
-   
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
+    const users = database.select("users");
+
+    return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
     const { name, email } = req.body;
 
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    });
+    };
+
+    database.insert("users", user);
 
     return res.writeHead(201).end();
   }
